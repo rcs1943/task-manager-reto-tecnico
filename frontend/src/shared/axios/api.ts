892cb03d8107bd731ec3717aad145aec8780b046
Ralf <kerwints6@gmail.com>
2026-04-17 11:33:03 -0500

    -

--- frontend/src/shared/axios/api.ts
+++ frontend/src/shared/axios/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import ApiInterceptorApplier from "./ApiInterceptorApplier";
 
 function createApi(port: string) {
-  const BASE_URL = __DEV__ ? `http://localhost:${port}` : process.env.EXPO_PUBLIC_API_URL;;
+  const BASE_URL = __DEV__ ? `http://localhost:${port}` : process.env.EXPO_PUBLIC_API_URL;
   return axios.create({
     baseURL: BASE_URL,
     timeout: Number(process.env.EXPO_PUBLIC_API_REQUEST_TIMEOUT) || 10000,
